e7b32f7b101308ed630214842096707b398abce8
Davide Riva <driva95@protonmail.com>
2020-04-12 21:26:32 +0200

    GPL comment added

--- code/similarity-graph/similarity_graph.py
+++ code/similarity-graph/similarity_graph.py
@@ -1,5 +1,20 @@
 '''Compute the similarity graph.'''
 
+# Copyright (C) 2020  Davide Riva <driva95@protonmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import argparse
 import gc
 import logging
@@ -176,7 +191,7 @@ def hellinger_parallel(doctopic, res):
 
 def plot(sim_graph):
     '''Plot the similarity graph.'''
-    plt.imshow(sim_graph)
+    plt.imshow(sim_graph, cmap='YlOrBr_r')
     plt.colorbar()
     plt.title(ARGS.filename)
     plt.savefig(ARGS.filename + '.svg')
