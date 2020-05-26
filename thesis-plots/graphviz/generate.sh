3d5fc9cf9a697685a0f561292019ed77da9eda97
Davide95 <driva95@protonmail.com>
2020-05-26 20:58:34 +0200

    Graphviz output is now PDF

--- thesis-plots/graphviz/generate.sh
+++ thesis-plots/graphviz/generate.sh
@@ -1,3 +1,3 @@
-dot -Tpng lda.txt -o ../../thesis/images/lda.png
-dot -Tpng vi.txt -o ../../thesis/images/lda-vi.png
-dot -Tpng hdp.txt -o ../../thesis/images/hdp.png
+dot -Tpdf lda.txt -o ../../thesis/images/lda.pdf
+dot -Tpdf lda-vi.txt -o ../../thesis/images/lda-vi.pdf
+dot -Tpdf hdp.txt -o ../../thesis/images/hdp.pdf
