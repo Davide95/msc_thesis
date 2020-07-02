d0755b0a97f7a169abd20b22140feb18c93aa52e
Davide95 <driva95@protonmail.com>
2020-07-02 18:25:43 +0200

    Preprocessing tests changed according to the new edits

---
+++ tests/doctopic/test.sh
@@ -0,0 +1,1 @@
+python doctopic.py in.csv english --plot_topics plot_topics.svg --dataset_id test
--- tests/similarity_graph/test.sh
+++
@@ -1,1 +0,0 @@
-python similarity_graph.py in.csv english --plot_sim plot_sim.svg --plot_topics plot_topics.svg --dataset_id test
