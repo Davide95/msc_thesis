d38d6185111f54678bcabf5671de8ac90b6fab30
Davide95 <driva95@protonmail.com>
2020-07-08 19:07:22 +0200

    Output format changed due to performance reasons

--- code/binarize.py
+++ code/binarize.py
@@ -18,45 +18,29 @@
 import argparse
 from pathlib import Path
 
-import networkx as nx
 import numpy as np
-import pandas as pd
 
 
 def binarize():
-    print('Loading distances matrix...')
-    data = 1 - np.load(ARGS.distances_matrix)
+    data = 1 - np.load(ARGS.filename)
     binarized = data >= ARGS.threshold
+    np.fill_diagonal(binarized, False)
 
-    print('Creation of the graph...')
-    return nx.from_numpy_matrix(binarized)
-
-
-def assign_labels(content):
-    print('Loading scraped data...')
-    structure = pd.read_csv(ARGS.scraped_data, usecols=['url'])
-
-    print('Relabelling nodes...')
-    labels = structure['url'].values
-    indexes = list(structure.index)
-    mapping = dict(zip(indexes, labels))
-    nx.relabel_nodes(content, mapping, copy=False)
+    return binarized
 
 
 # The execution starts here
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser()
-    PARSER.add_argument('distances_matrix',
+    PARSER.add_argument('filename',
                         help='filename of the .npy containing the distance matrix')
-    PARSER.add_argument('scraped_data',
-                        help='filename of the CSV containing the scraped data')
     PARSER.add_argument(
         'threshold', type=float,
         help='Threshold found through the random_threshold step')
     ARGS = PARSER.parse_args()
 
     NETWORK = binarize()
-    assign_labels(NETWORK)
+    PATH = Path(ARGS.filename).stem + '-adj.npy'
 
-    print('Saving data...')
-    nx.write_gml(NETWORK, Path(ARGS.distances_matrix).stem + '.gml')
+    np.save(PATH, NETWORK)
+    print('Output saved at:', PATH)
